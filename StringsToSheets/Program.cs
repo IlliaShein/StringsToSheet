a3aeb50d81b9c1f88eda835ea2768f974db36f92
IlliaShein <illiasheinillia@gmail.com>
2023-10-08 04:50:40 +0300

    Added 'CreateSheet' method and try/catch in MapPost

--- StringsToSheets/Program.cs
+++ StringsToSheets/Program.cs
@@ -4,9 +4,9 @@ using System.Text.Json;
 
 namespace StringsToSheets
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
@@ -36,25 +36,37 @@ namespace StringsToSheets
                     requestBody = await reader.ReadToEndAsync();
                 }
 
-                string[] strings = JsonSerializer.Deserialize<string[]>(requestBody);
-
-
-                string path = Path.Combine(Directory.GetCurrentDirectory(), "StringsSheet.xlsx");
-                var package = new ExcelPackage(new FileInfo(path));
-
-                ExcelWorksheet resultSheet = package.Workbook.Worksheets.Add("StringsSheet");
-
-                for (int i = 0; i < strings.Length; i++)
+                string[] strings;
+                try
                 {
-                    resultSheet.Cells[i + 1, 1].Value = strings[i];
+                    strings = JsonSerializer.Deserialize<string[]>(requestBody);
+                }
+                catch (JsonException)
+                {
+                    context.Response.StatusCode = 400;
+                    return;
                 }
 
-                package.Save();
+                CreateSheet(strings);
+
                 context.Response.StatusCode = 200;
             });
 
             app.Run();
         }
 
+        private static void CreateSheet(string[] strings)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "StringsSheet.xlsx");
+            var package = new ExcelPackage(new FileInfo(path));
+            ExcelWorksheet resultSheet = package.Workbook.Worksheets.Add("StringsSheet");
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                resultSheet.Cells[i + 1, 1].Value = strings[i];
+            }
+
+            package.Save();
+        }
     }
 }
